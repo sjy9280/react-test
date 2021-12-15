67662cefd7e384d97c4b9ccda4c2d54b6ae7e078
Joanna <2807902616@qq.com>
2021-12-15 18:52:13 +0800

    create countdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import AddStudent from "./Components/AddStudent";
-import StudentTitle from "./Components/StudentTitle";
-import StudentList from "./Components/StudentList";
 
 class App extends Component {
-    render() {
-        return (
-            <div className={ 'container' }>
-                <StudentTitle/>
-                <AddStudent/>
-                <StudentList/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={ 'container' }>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/countdown/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class countDown extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}

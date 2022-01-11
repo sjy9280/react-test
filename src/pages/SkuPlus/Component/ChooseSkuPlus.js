b1614b1772c94805bac521960e2d3e276c1e293f
Joanna <sjy9280@163.com>
2022-01-11 15:45:18 +0800

    feat:将无向图无权图改为无向有权图，解决因为闭环生成的新规格

--- src/pages/SkuPlus/Component/ChooseSkuPlus.js
+++ src/pages/SkuPlus/Component/ChooseSkuPlus.js
@@ -8,6 +8,8 @@ function ChooseSkuPlus(props) {
   const [inventoryState, setInventoryState] = useState(0)
   const [buyNumState, setBuyNumState] = useState(0)
   const [matrixState, setMatrixState] = useState([])
+  const [currentSelectState, setCurrentSelectState] = useState({})
+  const [specListState, setSpecListState] = useState([])
 
 
   const initSpec = () => {
@@ -33,6 +35,7 @@ function ChooseSkuPlus(props) {
       })
     })
     setSpecState(spec)
+    setSpecListState(specList)
     initMatrix(specList)
   }
 
@@ -51,48 +54,94 @@ function ChooseSkuPlus(props) {
 
   // 填充邻接矩阵
   const fillMatrix = (matrix, specList) => {
-    let edgeList = getAllPossiblePath()
-    let skuList = skus.filter(sku => sku.inventory > 0)
-    skuList.forEach(sku => {
+    let specKeys = specs.map(spec => spec.name)
+    let edgeList = getAllPossiblePath(specKeys)
+    skus.forEach((sku, skuIndex) => {
       edgeList.forEach(edge => {
-        let node1Key = edge[0];
-        let node2Key = edge[1];
+        let node1Key = edge[0]
+        let node2Key = edge[1]
         let node1Val
         sku.properties.map(item => {
           if (item.name === node1Key) node1Val = item.value
-        });
+        })
         let node2Val
         sku.properties.forEach(item => {
           if (item.name === node2Key) node2Val = item.value
-        });
-        let node1Index = specList.indexOf(node1Val);
-        let node2Index = specList.indexOf(node2Val);
-        matrix[node1Index][node2Index] = 1;
-        matrix[node2Index][node1Index] = 1;
-      });
-    });
-    addSameLevelEdge(matrix);
+        })
+        let node1Index = specList.indexOf(node1Val)
+        let node2Index = specList.indexOf(node2Val)
+        let cur = matrix[node1Index][node2Index]
+        if (typeof cur !== 'number') {
+          matrix[node1Index][node2Index].push(skuIndex + 2)
+        } else if (cur > 1) {
+          matrix[node1Index][node2Index] = [cur, skuIndex + 2]
+        } else {
+          matrix[node1Index][node2Index] = skuIndex + 2
+        }
+      })
+    })
+    addSameLevelEdge(matrix, specList)
   }
 
-  const addSameLevelEdge = (matrix) => {
+  // 添加同级可达路径
+  const addSameLevelEdge = (matrix, specList) => {
+    for (let i = 0; i < specs.length; i++) {
+      let specItemList = specs[i].values
+      specItemList = specItemList.filter(item => specList.indexOf(item) !== -1)
+      if (specItemList.length >= 2) {
+        let edgeList = getAllPossiblePath(specItemList)
+        edgeList.forEach(edge => {
+          let node1Index = specList.indexOf(edge[0])
+          let node2Index = specList.indexOf(edge[1])
+          let cur = matrix[node1Index][node2Index]
+          if (typeof cur !== 'number') {
+            matrix[node1Index][node2Index].push(1)
+          } else if (cur > 1) {
+            matrix[node1Index][node2Index] = [cur, 1]
+          } else {
+            matrix[node1Index][node2Index] = 1
+          }
+        })
+      }
+    }
     setMatrixState(matrix)
   }
 
   // 获取所有可能的边
-  const getAllPossiblePath = () => {
-    let specKeys = specs.map(spec => spec.name)
-    let pairs = [];
-    for (let i = 0; i < specKeys.length - 1; i++) {
-      for (let j = i + 1; j < specKeys.length; j++) {
+  const getAllPossiblePath = (specKeys) => {
+    let pairs = []
+    for (let i = 0; i < specKeys.length; i++) {
+      for (let j = 0; j < specKeys.length; j++) {
         pairs.push([specKeys[i], specKeys[j]])
       }
     }
-    console.log(pairs)
     return pairs
   }
 
-  const selectSpec = (key, value) => {
+  // 点击属性按钮
+  function selectSpec(key, index, value) {
+    let newSpecs = { ...specState }
+    let newCurrentSelected = { ...currentSelectState }
+    newSpecs[key][index].selected = !newSpecs[key][index].selected
+    if (newSpecs[key][index].selected) {
+      newSpecs[key].forEach((item, index1) => {
+        if (item.selected && index1 !== index) {
+          newSpecs[key][index1].selected = false
+        }
+      })
+      newCurrentSelected[key] = value
+    } else {
+      delete newCurrentSelected[key]
+    }
+    setCurrentSelectState(newCurrentSelected)
+    setSpecState(newSpecs)
 
+  }
+
+  // 计算不能够到达的路径
+  const calculateUnAvailableSpecItem = () => {
+    let unAvailableSpecItemList = []
+    let selectedCount = Object.keys(currentSelectState).length
   }
 
   useEffect(() => {
@@ -116,7 +165,7 @@ function ChooseSkuPlus(props) {
                           <button key={ index } className={ classNames('sku-btn', {
                             'selected': spec.selected,
                             'disabled': spec.disabled
-                          }) } onClick={ selectSpec(key, spec.value) }
+                          }) } onClick={ () => selectSpec(key, index, spec.value) }
                                   disabled={ spec.disabled }>{ spec.value }</button>
                         )
                       }) }
